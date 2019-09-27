715b152280822c48fce54bf2c48bbd4e9d625851
Noel Castillo <noelcastillo@Administrators-MacBook-Pro.local>
2019-09-27 14:17:31 -0600

    Chunk of coding completed

--- src/jets/AirField.java
+++ src/jets/AirField.java
@@ -9,17 +9,15 @@ public class AirField {
 
 	private Jet[] jets;
 
+	private static int jetNumber = 0;
+
 //	C O N S T R U C T O R S
 
+	public AirField() {
+
+	}
+
 //	M E T H O D S 
-
-	public Jet[] getJets() {
-		return jets;
-	}
-
-	public void setJets(Jet[] jets) {
-		this.jets = jets;
-	}
 
 	public int displayMenu(Scanner keyboard) {
 		int menuSelection = 0;
@@ -34,11 +32,54 @@ public class AirField {
 		System.out.println("8. Remove a jet from Fleet");
 		System.out.println("9. Quit");
 		try {
-		menuSelection = keyboard.nextInt();
-		} catch(InputMismatchException e){
+			menuSelection = keyboard.nextInt();
+		} catch (InputMismatchException e) {
 			System.out.println("Input invalid");
 		}
 		return menuSelection;
 	}
+	
+	public Jet addJetToFleet(Scanner keyboard) {
+		Jet output;
+		System.out.println("1. Cargo Plane");
+		System.out.println("2. Fighter Jet");
+		int jetType = keyboard.nextInt();
+		System.out.print("Enter model:");
+		String model = keyboard.nextLine();
+		keyboard.nextLine();
+		System.out.print("Enter speed:");
+		double speed = keyboard.nextDouble();
+		System.out.print("Enter range:");
+		int range = keyboard.nextInt();
+		System.out.print("Enter price:");
+		long price = keyboard.nextLong();
+		switch(jetType) {
+		case 1:
+			output = new CargoPlane(model, speed, range, price);
+			break;
+		case 2:
+			output = new FighterJet(model, speed, range, price);
+			break;
+		default:
+			output = null;
+			break;
+		}
+		return output;
+	}
 
+	public Jet[] getJets() {
+		return jets;
+	}
+
+	public void setJets(Jet[] jets) {
+		this.jets = jets;
+	}
+
+	public int getJetNumber() {
+		return jetNumber;
+	}
+
+	public void setJetNumber(int jetNumber) {
+		AirField.jetNumber = jetNumber;
+	}
 }
